e1185236575e9de0a9a5603e81c31da4bb47c348
Matthew Durand <matt.d@samsung.com>
2023-03-31 16:48:00 -0700

    Revert "Fixed the AWS SDK build script to use rpmbuild macros and build the RPM"
    This reverts commit e434107d95847e7307574a97e5bab91df511fdd5.

--- scripts/build_aws-sdk.sh
+++ scripts/build_aws-sdk.sh
@@ -2,12 +2,36 @@ set -e
 
 GIT_AWS_RELEASE=1.9
 SCRIPT_DIR=$(realpath ${PWD}/$(dirname $0))
+TOP_DIR=${PWD}/aws-build-$(date +%s)/
+INSTALL_DIR=${TOP_DIR}/BUILDROOT/aws-sdk-cpp-${GIT_AWS_RELEASE}-0.x86_64/usr/local
 AWS_SPEC_FILE="aws-${GIT_AWS_RELEASE}.spec"
 AWS_DIR="aws-git-${GIT_AWS_RELEASE}"
 GIT_CHECKOUT_TAG="1.9.343-elbencho-tag"
 
-cp aws-git-${GIT_AWS_RELEASE}.patch $HOME/rpmbuild/SOURCES/
+#Cleanup old directories
+rm -rf $TOP_DIR
+rm -rf $AWS_DIR
 
+mkdir -p $INSTALL_DIR
+
+# Do git clone and build
+#git clone --recurse-submodules https://github.com/aws/aws-sdk-cpp.git ${AWS_DIR}
+#git checkout release/${GIT_AWS_RELEASE}
+git clone --recursive https://github.com/breuner/aws-sdk-cpp.git ${AWS_DIR}
+git checkout ${GIT_CHECKOUT_TAG}
+
+pushd ${AWS_DIR}
+
+# Apply patches for building AWS
+patch -p1 < ${SCRIPT_DIR}/aws-git-${GIT_AWS_RELEASE}.patch
+
+# Compiling the code
+source /opt/rh/devtoolset-11/enable
+cd crt/aws-crt-cpp
+cmake3 . -DBUILD_SHARED_LIBS=ON -DCPP_STANDARD=17 -DAUTORUN_UNIT_TESTS=OFF -DENABLE_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DBYO_CRYPTO=ON -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} && make -j $(nproc) install
+cd ../..
+cmake3 . -DBUILD_ONLY="s3;transfer" -DBUILD_SHARED_LIBS=ON -DCPP_STANDARD=17 -DAUTORUN_UNIT_TESTS=OFF -DENABLE_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DBYO_CRYPTO=ON -DBUILD_DEPS=OFF -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} && make -j $(nproc) install
+popd
 
 # Create spec file for RPM build
 cat > ${AWS_SPEC_FILE} << EOF
@@ -17,7 +41,6 @@ Release:        0
 Summary:        Amazon Web Services SDK for C++
 License:        ASL 2.0
 URL:            https://github.com/aws/%{name}
-Patch0:		aws-git-${GIT_AWS_RELEASE}.patch
 
 
 %define _unpackaged_files_terminate_build 0
@@ -28,22 +51,6 @@ AWS. It is meant to be performant and fully functioning with low- and
 high-level SDKs, while minimizing dependencies and providing platform
 portability (Windows, OSX, Linux, and mobile).
 
-%prep
-rm -rf aws-sdk-cpp
-git clone --recursive -q https://github.com/breuner/aws-sdk-cpp.git 
-cd aws-sdk-cpp
-git checkout ${GIT_CHECKOUT_TAG}
-
-%patch0 -p1
-
-%build
-# Compiling the code
-source /opt/rh/devtoolset-11/enable
-cd %{_builddir}/aws-sdk-cpp/crt/aws-crt-cpp
-cmake3 . -DBUILD_SHARED_LIBS=ON -DCPP_STANDARD=17 -DAUTORUN_UNIT_TESTS=OFF -DENABLE_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DBYO_CRYPTO=ON -DCMAKE_INSTALL_PREFIX=%{buildroot}/usr/local/ && make -j $(nproc) install
-cd ../..
-cmake3 . -DBUILD_ONLY="s3;transfer" -DBUILD_SHARED_LIBS=ON -DCPP_STANDARD=17 -DAUTORUN_UNIT_TESTS=OFF -DENABLE_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DBYO_CRYPTO=ON -DBUILD_DEPS=OFF -DCMAKE_INSTALL_PREFIX=%{buildroot}/usr/local/ && make -j $(nproc) install
-
 %files
 /usr/local/lib64/*
 /usr/local/include/*
@@ -52,5 +59,5 @@ EOF
 
 # Build AWS RPM
 echo "Building AWS RPM ...."
-rpmbuild -bb ${AWS_SPEC_FILE} 
+rpmbuild --define "_topdir ${TOP_DIR}" -bb ${AWS_SPEC_FILE} 
 
