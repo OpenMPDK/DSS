599810d1014327e324cef62776b4b8c103319f11
Matthew Durand <matt.d@samsung.com>
2023-04-07 16:08:37 -0700

    Support multi-branch builds (#24)
    * Rename script to stagemergeartifacts.sh
    * Allow artifacts to be stored for any branch merge
    * Update URI secret name
    * Improve comments

--- scripts/stagemergeartifacts.sh
+++ scripts/stagemergeartifacts.sh
@@ -2,14 +2,15 @@
 set -e
 # stagemainartifacts.sh
 #
-# Identify newly-built artifacts, then stage them in the 'main' folder of the artifacts bucket.
-# If existing 'main' artifacts exist, remove them first.
+# Identify newly-built artifacts, then stage them in a folder of the artifacts bucket.
+# This folder will match the branch name of the merge.
+# If artifacts exist in this folder matching a file glob, remove them first.
 #
 # This script will only execute when triggered by a GitHub merge event.
 # This is determined by the presence of the substring 'refs/heads/' in a $GITHUB_REF var.
 #
 # This script must be passed the following vars:
-#   DSSMAINS3URI:   The S3 URI pointing to the artifacts bucket
+#   DSSS3URI:   The S3 URI pointing to the artifacts bucket
 #   DSSGLOBLIST:    A list of fileglobs expected to be present after a successful build.
 #                       Note if any glob does not match, the script will fail and return non-zero.
 #
@@ -17,10 +18,10 @@ set -e
 # Read DSSGLOBLIST var as array
 IFS=' ' read -ra DSSGLOBLIST <<< "$DSSGLOBLIST"
 
-# Check if DSSMAINS3URI is defined
-if [[ "$DSSMAINS3URI" == '' ]]
+# Check if DSSS3URI is defined
+if [[ "$DSSS3URI" == '' ]]
 then
-    echo "*** ERROR: DSSMAINS3URI var not defined"
+    echo "*** ERROR: DSSS3URI var not defined"
     exit 1
 fi
 
@@ -32,11 +33,14 @@ then
 fi
 
 # Check if build was triggered by merge. Otherwise skip (eg: skip PR or manual build)
-if [[ $GITHUB_REF != *"refs/heads"* ]]
+if [[ $GITHUB_REF != "refs/heads"* ]]
 then
     echo "Not a GitHub merge. Skipping main artifacts rotation."
     exit 0
 fi
+
+# Derive the branch name
+BRANCH_NAME=$(echo "$GITHUB_REF" | cut --complement -c 1-11)
 
 # Find local artifacts
 LOCALARTIFACTS=()
@@ -57,7 +61,7 @@ done
 for GLOB in "${DSSGLOBLIST[@]}"
 do
     set +e
-    mapfile -t REMOTEARTIFACTS < <(aws s3 ls "$DSSMAINS3URI"/ | grep -oP "${GLOB//\*/.*}")
+    mapfile -t REMOTEARTIFACTS < <(aws s3 ls "$DSSS3URI/$BRANCH_NAME/" | grep -oP "${GLOB//\*/.*}")
     set -e
 
     if [[ "${REMOTEARTIFACTS[0]}" == '' ]]
@@ -67,7 +71,7 @@ do
         for artifact in "${REMOTEARTIFACTS[@]}"
         do
             echo "Deleting existing object from artifacts bucket: $artifact"
-            aws s3 rm "$DSSMAINS3URI/$artifact" --only-show-errors
+            aws s3 rm "$DSSS3URI/$BRANCH_NAME/$artifact" --only-show-errors
         done
     fi
 done
@@ -76,5 +80,5 @@ done
 for file in "${LOCALARTIFACTS[@]}"
 do
     echo "Copying file to artifacts bucket: $file"
-    aws s3 cp "$file" "$DSSMAINS3URI"/ --only-show-errors
+    aws s3 cp "$file" "$DSSS3URI/$BRANCH_NAME/" --only-show-errors
 done
