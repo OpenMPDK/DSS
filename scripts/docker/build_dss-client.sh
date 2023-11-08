504cca96d25564d5e386f27e7981bec15ece3d15
Matthew Durand <matt.d@samsung.com>
2023-11-07 16:26:04 -0800

    Build client with user-mode

--- scripts/docker/build_dss-client.sh
+++ scripts/docker/build_dss-client.sh
@@ -3,4 +3,4 @@ set -e
 
 # Build aws-sdk-cpp in Docker
 DOCKER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-"$DOCKER_DIR"/build_docker.sh -c dss-client -d DSS -t dssbuild -a 'dss_client-*.tgz'
+"$DOCKER_DIR"/build_docker.sh -c dss-client -d DSS -t dssbuild -a 'dss_client-*.tgz' -u
